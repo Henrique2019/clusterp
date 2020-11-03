4d613c9cc7900a6ad3f040870c80a6b37580aff1
Henrique2019 <48299527+Henrique2019@users.noreply.github.com>
2020-11-03 14:39:58 -0300

    editando add

--- clusterpteste/src/app/app.module.ts
+++ clusterpteste/src/app/app.module.ts
@@ -2,18 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
-
-import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 import { AddComponent } from './componentes/add/add.component';
 import { ListaComponent } from './componentes/lista/lista.component';
+import { InputMatComponent } from './shared/input-mat/input-mat.component';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     AddComponent,
     ListaComponent,
+    InputMatComponent,
 
   ],
   imports: [
@@ -21,7 +25,10 @@ import { ListaComponent } from './componentes/lista/lista.component';
     FormsModule,
     HttpClientModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    BrowserAnimationsModule,
+    MaterialModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- clusterpteste/src/app/shared/input-mat/input-mat.component.ts
+++ clusterpteste/src/app/shared/input-mat/input-mat.component.ts
@@ -2,19 +2,21 @@ import { Component, OnInit } from '@angular/core';
 import { SubGrupoModel } from 'src/app/models/sub-grupo-model';
 import { ClustService } from 'src/app/service/clust.service';
 import { NgForm } from '@angular/forms';
+/**
+ * @title Input with hints
+ */
 
 @Component({
-  selector: 'app-add',
-  templateUrl: './add.component.html',
-  styleUrls: ['./add.component.css']
+  selector: 'app-input-mat',
+  templateUrl: './input-mat.component.html',
+  styleUrls: ['./input-mat.component.css']
 })
-export class AddComponent implements OnInit {
+export class InputMatComponent implements OnInit {
 
   Clust = {} as SubGrupoModel;
   Clusts: SubGrupoModel[];
 
   constructor(private ClustService: ClustService) {}
-
 
   ngOnInit() {
     this.getClusts();
@@ -32,31 +34,17 @@ export class AddComponent implements OnInit {
       });
     }
   }
-
-  // Chama o serviço para obtém todos os Item
+  // Chama o serviço para obtém todos os Itens
   getClusts() {
     this.ClustService.getClusts().subscribe((Clusts: SubGrupoModel[]) => {
       this.Clusts = Clusts;
     });
   }
-
-  // deleta um Item
-  deleteClusts(Clust: SubGrupoModel) {
-    this.ClustService.deleteClusts(Clust).subscribe(() => {
+    // limpa o formulario
+    cleanForm(form: NgForm) {
       this.getClusts();
-    });
-  }
-
-  // copia o Item para ser editado.
-  editClusts(Clust: SubGrupoModel) {
-    this.Clust = { ...Clust };
-  }
-
-  // limpa o formulario
-  cleanForm(form: NgForm) {
-    this.getClusts();
-    form.resetForm();
-    this.Clust = {} as SubGrupoModel;
-  }
+      form.resetForm();
+      this.Clust = {} as SubGrupoModel;
+    }
 
 }
